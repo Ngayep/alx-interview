236dfde39bcbbc62632c625532e9eef6cfbf7353
Ngayep <courtesyofjess@gmail.com>
2024-10-15 09:28:28 +0100

    documentation

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ method that determines if all the boxes can be opened."""
 
+
 def canUnlockAll(boxes):
     """
     Determines if all boxes can be opened starting from box 0.

aa5b43a68e80d5e8d5dc9fbc9e722c2d742857bc
Ngayep <courtesyofjess@gmail.com>
2024-10-27 13:37:04 +0100

    logparse

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,46 +1,48 @@
 #!/usr/bin/python3
 
-""" script that reads stdin line by line and computes metrics """
+"""Script that reads stdin line by line and computes metrics."""
 
 import sys
 
-
 def printStatus(dic, size):
-    """ Prints information """
+    """Prints accumulated file size and status code counts."""
     print("File size: {:d}".format(size))
-    for i in sorted(dic.keys()):
-        if dic[i] != 0:
-            print("{}: {:d}".format(i, dic[i]))
+    for code in sorted(dic.keys()):
+        if dic[code] != 0:
+            print("{}: {:d}".format(code, dic[code]))
 
-
-# sourcery skip: use-contextlib-suppress
-statusCodes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
-               "404": 0, "405": 0, "500": 0}
+statusCodes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
 
 count = 0
 size = 0
 
 try:
     for line in sys.stdin:
-        if count != 0 and count % 10 == 0:
-            printStatus(statusCodes, size)
-
-        stlist = line.split()
         count += 1
+        stlist = line.split()
 
         try:
+            # Add the file size (last item in the line)
             size += int(stlist[-1])
-        except Exception:
+        except (IndexError, ValueError):
             pass
 
         try:
+            # Update status code count if it exists in our dictionary
             if stlist[-2] in statusCodes:
                 statusCodes[stlist[-2]] += 1
-        except Exception:
+        except IndexError:
             pass
-    printStatus(statusCodes, size)
 
+        # Print status every 10 lines
+        if count % 10 == 0:
+            printStatus(statusCodes, size)
+
+    # Print final stats if remaining lines after the last multiple of 10
+    printStatus(statusCodes, size)
 
 except KeyboardInterrupt:
+    # Print stats on manual interruption
     printStatus(statusCodes, size)
     raise
+
